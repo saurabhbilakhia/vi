427177aaa4731afe8b586f69e1670448980f5d98
Saurabh <sbilakhia.developer@gmail.com>
2019-01-01 14:54:05 -0500

    cleanup

--- prod.config.sh
+++
@@ -1,4 +0,0 @@
-export aws_rds_host = 'valueinvestingproddb.cdu3sla5f5lz.us-east-1.rds.amazonaws.com'
-export aws_rds_port = 3306
-export aws_rds_user = 'sbilakhia'
-export aws_rds_password = 'Value!nve$ting'
